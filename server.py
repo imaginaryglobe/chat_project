e6a31de03282d8718396627eabbe1e0ffc00d9ad
Jaden Fang <jadenfang9@gmail.com>
2026-01-25 17:37:30 +0000

    Allow users to join from more terminals and with 'username 's

--- server.py
+++ server.py
@@ -5,12 +5,15 @@ import threading
 def new_client_connection(conn, add):
     while True:
         buf = conn.recv(64)
+        if not buf:
+            break;
         buf = buf.decode()
-        if buf == "close server":
+        if buf == "exit":
             break
         else:
             messages.append(buf)
-            print(buf)
+            print(add, "says:", buf)
+
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serversocket.bind(('localhost', 8089))
 serversocket.listen(2) 
@@ -21,11 +24,3 @@ while True:
     connection, address = serversocket.accept()
     thread = threading.Thread(target=new_client_connection, args=(connection, address))
     thread.start()
-
-    buf = connection.recv(64)
-    buf = buf.decode()
-    if buf == "close server":
-        break
-    else:
-        messages.append(buf)
-        print(buf)
